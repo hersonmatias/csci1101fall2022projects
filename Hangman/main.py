45a8f9657993052c8ba70a40b25fff4428512126
Herson_Matias <94094990+hersonmatias@users.noreply.github.com>
2022-10-17 14:01:35 -0500

    Update main.py

--- Hangman/main.py
+++ Hangman/main.py
@@ -16,3 +16,10 @@ for current_answer_character in answer:
     else:
         answer_guessed.append(True)
 
+# Game logic.
+current_incorrect_guesses = 0
+
+while current_incorrect_guesses < num_of_incorrect_guesses or False not in answer_guessed:
+    print(f"Number of incorrect guesses left: {num_of_incorrect_guesses - current_incorrect_guesses}")
+
+    #Display puzzle board.
