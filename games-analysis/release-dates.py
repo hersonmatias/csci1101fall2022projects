7bc7de15c7a5942d49997303fdebb2f9ded85a4d
Herson_Matias <94094990+hersonmatias@users.noreply.github.com>
2022-11-28 13:44:02 -0600

    add data science project

---
+++ games-analysis/release-dates.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("game-ratings-by-release-dates.csv")
+
+# Clean up fields.
+df["first_release_date"] = pd.to_datetime(df["first_release_date"])
+
+# Analyze data.
+plt.scatter(df["first_release_date"], df["critic_rating_value"], color = "blue", label = "Critic Rating")
+plt.scatter(df["first_release_date"], df["user_rating_value"], color = "red", label = "User Rating")
+
+plt.legend(loc = "upper left")
+
+plt.show()
+
